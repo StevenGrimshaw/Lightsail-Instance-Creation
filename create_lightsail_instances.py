c065a2867da4c9519723a5d94b204111d11055c8
Steven Grimshaw <stevengrimshaw@tamu.edu>
2022-09-15 17:19:33 -0500

    Created app for creating instances

---
+++ create_lightsail_instances.py
@@ -0,0 +1,28 @@
+import boto3, uuid
+
+client = boto3.client(
+    'lightsail', 
+    region_name='us-east-2',
+    aws_access_key_id='AKIATDOVHAF4KWTJ4YOX', 
+    aws_secret_access_key='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')
+
+NUM_INSTANCES = 0
+while True:
+    inp = input("Enter the number of instances you would like to create: ")
+    if not inp.isdigit() or int(inp) <= 0:
+        print("ERROR: Input must be a positive integer")
+        continue
+    NUM_INSTANCES = int(inp)
+    break
+
+for i in range(0, NUM_INSTANCES):
+    INSTANCE_NAME = 'GeneratedVPC-' + str(uuid.uuid4())
+    client.create_instances_from_snapshot(
+        instanceNames=[
+            INSTANCE_NAME,
+        ],
+        availabilityZone='us-east-2a',
+        instanceSnapshotName='syslink-Windows_Server_2019-1-1663276007',
+        bundleId='nano_win_2_0'
+    )
+    print("Generated VPC (%s): %s" % ((i + 1), INSTANCE_NAME))
